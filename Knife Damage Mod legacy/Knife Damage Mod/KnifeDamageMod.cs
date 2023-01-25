537d381e926475d62bcf2421e4836988e37e0d09
Aftershock3995 <121205606+Aftershock3995@users.noreply.github.com>
2023-01-25 16:31:05 -0500

    Add files via upload

---
+++ Knife Damage Mod legacy/Knife Damage Mod/KnifeDamageMod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using KnifeDamageMod_SN;
+using UnityEngine;
+using Logger = QModManager.Utility.Logger;
+
+namespace KnifeDamageMod_SN
+{
+    internal class KnifeDamageMod
+    {
+        [HarmonyPatch(typeof(PlayerTool))]
+        [HarmonyPatch("OnDraw")]
+        internal class PatchPlayerToolAwake
+
+        {
+            [HarmonyPostfix]
+
+            public static void Postfix(PlayerTool __instance)
+            {
+                if (__instance.GetType() == typeof(Knife))
+                {
+                    Knife knife = __instance as Knife;
+
+                    float damage = QMod.config.knifeDamage;
+                    float reach = QMod.config.knifeRange;
+
+                    knife.damage = damage * 25f;
+                    knife.attackDist = reach * 2f;
+                }
+                else if (__instance.GetType() == typeof(HeatBlade))
+                {
+                    HeatBlade heatblade = __instance as HeatBlade;
+
+                    heatblade.attackDist = QMod.config.knifeRange * 2f;
+                    heatblade.damage = QMod.config.knifeDamage * 25f;
+                }
+            }
+        }
+    }
+}
