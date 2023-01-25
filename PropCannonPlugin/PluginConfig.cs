d1a20f04d2695a874acf2df4f5b6ea7673694313
Aftershock3995 <121205606+Aftershock3995@users.noreply.github.com>
2023-01-25 16:27:44 -0500

    Add files via upload

---
+++ PropCannonPlugin/PluginConfig.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx.Configuration;
+
+namespace PluginConfigs
+{
+    public static class PluginConfig
+    {
+        public static void Initialize(ConfigFile cfg)
+        {
+            maxMass = cfg.Bind(
+                section: "Propulsion Cannon Settings",
+                key: "Cannon Max Pickup Mass",
+                defaultValue: 1200f,
+                new ConfigDescription(
+                   description: "Set the max mass the cannon can pickup",
+                   acceptableValues: new AcceptableValueRange<float>(1f, 10000f),
+                   tags: new ConfigurationManagerAttributes { Order = 1 }
+            )
+        );
+            pickupDistance = cfg.Bind(
+                section: "Propulsion Cannon Settings",
+                key: "Cannon Max Pickup Distance",
+                defaultValue: 18f,
+                new ConfigDescription(
+                    description: "Set the max distance the cannon can pickup",
+                    acceptableValues: new AcceptableValueRange<float>(1f, 1000f),
+                    tags: new ConfigurationManagerAttributes { Order = 2 }
+            )
+        );
+            shootForce = cfg.Bind(
+                section: "Propulsion Cannon Settings",
+                key: "Cannon Max Shoot Force",
+                defaultValue: 50f,
+                new ConfigDescription(
+                    description: "Set the amount of shooting force",
+                    acceptableValues: new AcceptableValueRange<float>(1f, 1000f),
+                    tags: new ConfigurationManagerAttributes { Order = 3 }
+            )
+        );
+
+        }
+
+        internal static ConfigEntry<float> maxMass;
+        internal static ConfigEntry<float> pickupDistance;
+        internal static ConfigEntry<float> shootForce;
+    }
+}
---
+++ PropCannonPlugin/PropulsionCannonPlugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+using HarmonyLib;
+using PluginConfigs;
+
+namespace Aftershock
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class Main : BaseUnityPlugin
+    {
+        #region BEPINEX
+        private const string myGUID = "com.Aftershock3995.AftershockCannon";
+        private const string pluginName = "Propulsion Cannon Plugin";
+        private const string versionString = "1.0.0";
+        private static readonly Harmony harmony = new Harmony(myGUID);
+        internal static ManualLogSource logger;
+        #endregion
+
+        public void Awake()
+
+        {
+            harmony.PatchAll();
+            logger = Logger;
+            PluginConfig.Initialize(Config);
+        }
+    }
+  
+    [HarmonyPatch(typeof(PropulsionCannon), nameof(PropulsionCannon.OnDisable))]
+
+    public class PropulsionCannon_Update_Patch
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(PropulsionCannon __instance)
+        {
+            __instance.pickupDistance = PluginConfig.pickupDistance.Value;
+            __instance.maxMass = PluginConfig.maxMass.Value;
+            __instance.shootForce = PluginConfig.shootForce.Value;
+
+        }
+
+    }
+}
